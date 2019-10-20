49ab981468306b1382058fcd9533434cf5d74b07
Yudu Ban <banyudu@gmail.com>
2019-10-21 05:18:44 +0800

    feat: implement main function

--- index.ts
+++ index.ts
@@ -1,6 +1,48 @@
 import * as util from 'util'
-import * as npm from 'npm'
+import npm from 'npm'
 
-export default async function npmLatestVersion () {
+export interface Options {
+  registry?: string;
+  modules: string[];
+  since?: Date;
+}
 
+export interface Result {
+  [distTag: string]: {
+    version: string;
+    time: Date;
+  }
+}
+
+export default async function npmLatestVersion (options: Options): Promise<Result> {
+  return new Promise((resolve, reject) => {
+    let npmOptions: any = {}
+    if (options.registry) {
+      npmOptions.registry = options.registry
+    }
+    npm.load(npmOptions, () => {
+      npm.commands.show(options.modules, true, (err, res) => {
+        if (err) {
+          reject(err)
+        } else {
+          let versionInfo = res[Object.keys(res)[0]]
+
+          const result: Result = {}
+
+          const distTags = versionInfo['dist-tags']
+          for (let tag in distTags) {
+            const version = distTags[tag]
+            const time = versionInfo.time[version]
+            if (time && (!options.since || new Date(time) > new Date(options.since))) {
+              result[tag] = {
+                version,
+                time
+              }
+            }
+          }
+          resolve(result)
+        }
+      })
+    })
+  })
 }
--- test/index.ts
+++ test/index.ts
@@ -1,2 +1,10 @@
 import * as assert from 'power-assert'
-import npmLatestVersion from '../'
+import npmLatestVersion from '../index'
+
+(async () => {
+  const result = await npmLatestVersion({
+    modules: ['express'],
+    since: new Date('2019/01/01')
+  })
+  console.log('result is: ', result)
+})()
